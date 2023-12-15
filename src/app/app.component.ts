ef47391abac31db1e8b09bb2bbf4597c3def47e4
Cristian <cristianndres@unicauca.edu.co>
2023-12-15 16:25:08 -0500

    Mockup screen course angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Screen1Component } from './screen-1/screen-1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, Screen1Component],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'pantallas-curso';
